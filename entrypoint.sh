7d0d2c5d9a54907ec1b4f13c2f497e952191642d
William Chanrico <williamchanrico@gmail.com>
2019-06-18 17:12:09 +0700

    Use alpine and refactor entrypoint using env to trigger the wait

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-echo Starting fortio "$@"
+# This entrypoint will wait for istio-proxy to be available
+
+if [ "$WAIT_FOR_ISTIO_SIDECAR" = true ]; then
+	echo Waiting for Sidecar
+
+	count=30
+	until curl -I localhost:15000; do
+		echo Still waiting...
+		count=$((count - 1))
+		sleep 2
+
+		if [ $count -lt 0 ]; then
+			echo "Bypassing sidecar check, took too long (60s++)"
+			break
+		fi
+	done
+fi
+
+echo Starting fortio "$@" in 10s
+sleep 10
 /usr/bin/fortio "$@"
